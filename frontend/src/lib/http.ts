15c911c097c044d6c6eadb9d5d667bd59c38eb22
Suleyman <suleyman@192.168.1.103>
2026-01-28 12:26:44 +0300

    fix(frontend): remove unused import

--- frontend/src/lib/http.ts
+++ frontend/src/lib/http.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError } from "axios";
 import type { InternalAxiosRequestConfig } from "axios";
 
 import { env } from "../config/env";
-import { detectHostType, isDevelopment } from "./hostDetection";
+import { detectHostType } from "./hostDetection";
 import { tokenStorage } from "./tokenStorage";
 import { errorLogger, ErrorSeverity } from "./errorLogger";
 
